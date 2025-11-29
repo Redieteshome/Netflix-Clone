06b0a53ed6bb77febb09673c0abfcf6b114210ef
Rediet Teshome Tadesse <redieteshome78@gmail.com>
2025-11-28 17:20:26 -0800

    row

--- netflix-clone/src/components/Banner/Banner.jsx
+++ netflix-clone/src/components/Banner/Banner.jsx
@@ -45,7 +45,7 @@ function Banner() {
           </div>
           <h1 className='banner_description'>{truncate(movie?.overview, 150)}</h1>
         </div>
-        <div className='banner_fadeBottom'  />
+        <div className='fadebottom'/>
       </div>
     </>
   );
